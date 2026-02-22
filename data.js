d8a69dbb5dd7de3b10db40684363ec96e0e343ca
abiola boluwatife <abiolaboluwatife@gmail.com>
2026-02-22 22:24:56 +0100

    a simple music player

---
+++ data.js
@@ -0,0 +1,81 @@
+export const musicPlayer=[
+    {
+        artistName:'ayrastar',
+        songTitle:'my love',
+        song:'music/ayrastar.m4a',
+        images:'images/ayra.jpeg'
+    },
+       {
+        artistName:'chella',
+        songTitle:'my darling',
+        song:'music/chella.m4a',
+        images:'images/chella.jpeg'
+    },
+       {
+        artistName:'dj tunes, fola, wizkid',
+        songTitle:'one condition',
+        song:'music/dj-tunes.m4a',
+        images:'images/tunes.jpeg'
+    },
+       {
+        artistName:'davido',
+        songTitle:'with you',
+        song:'music/davido.m4a',
+        images:'images/davido.jpeg'
+    },
+      {
+        artistName:'pheels',
+        songTitle:'finnesse',
+        song:'music/finnesse-pheels.m4a',
+        images:'images/pheels-pics.jpg'
+    },
+      {
+        artistName:'dj tunes wizkid fola',
+        songTitle:'one condition',
+        song:'music/dj-tunes.m4a',
+        images:'images/tunes.jpeg'
+    },
+        {
+        artistName:'fireboy',
+        songTitle:'love me',
+        song:'music/fireboysong.m4a',
+        images:'images/fireboy.jpeg'
+    },
+        {
+        artistName:'fola and kizz daniel',
+        songTitle:'lost',
+        song:'music/fola-danielsong.m4a',
+        images:'images/kiss.jpeg'
+    },
+        {
+        artistName:'fola',
+        songTitle:'eko',
+        song:'music/fola-music.m4a',
+        images:'images/fola.jpeg'
+    },
+        {
+        artistName:'young john and rema',
+        songTitle:'lalala',
+        song:'music/john-remasong.m4a',
+        images:'images/rema.jpeg'
+    },
+        {
+        artistName:'mavo',
+        songTitle:'skalabulizy',
+        song:'music/mavo-music.m4a',
+        images:'images/mavo.jpeg'
+    },
+        {
+        artistName:'pocolee and shoday',
+        songTitle:'hey jago',
+        song:'music/pocolee-shoday.m4a',
+        images:'images/shoday-poco.png'
+    },
+        {
+        artistName:'rema ',
+        songTitle:'fun',
+        song:'music/rema-fun.m4a',
+        images:'images/rema.jpeg'
+    },
+
+]
---
+++ music.js
@@ -0,0 +1,160 @@
+import { musicPlayer } from "./data.js"
+
+
+
+
+
+
+let playBtn=document.querySelector('.play')
+let pause=document.querySelector('.pause')
+let artistname=document.querySelector('.artist-name')
+let songtitle=document.querySelector('.music-title')
+let songRange=document.querySelector('.range-music')
+let prevBtn=document.querySelector('.prev-btn')
+let forwardBtn=document.querySelector('.forward-btn')
+let img=document.querySelector('.img')
+let displayError=document.querySelector('.display-error')
+let progress=document.querySelector('.range-music')
+let currentTime=document.querySelector('.current-time')
+let duration=document.querySelector('.duration')
+let list=document.querySelector('.list')
+let faShow=document.querySelector('.fa-bars')
+
+let audio=document.createElement('audio')
+
+
+faShow.addEventListener('click',()=>{
+    if(list.classList.contains('display')){
+list.classList.remove('display')
+
+    }
+    else{
+        list.classList.add('display')
+    }
+})
+
+
+let currentSongIndex=0
+updateSong()
+
+prevBtn.addEventListener('click',()=>{
+    if(currentSongIndex==0){
+       
+        return;
+    }
+  
+   currentSongIndex--;
+   updateSong()
+audio.play()
+ playBtn.innerHTML='<i class="fa fa-pause" aria-hidden="true"></i>'
+})
+
+forwardBtn.addEventListener('click',()=>{
+    if(currentSongIndex==musicPlayer.length - 1){
+        return;
+    }
+      
+    currentSongIndex++;
+  updateSong()
+audio.play()
+ playBtn.innerHTML='<i class="fa fa-pause" aria-hidden="true"></i>'
+})
+
+playBtn.addEventListener('click',()=>{
+    if(!audio.paused){
+        audio.pause()
+ playBtn.innerHTML='<i class="fa fa-play" aria-hidden="true"></i>'
+    }
+    else{
+  audio.play()
+
+     playBtn.innerHTML='<i class="fa fa-pause" aria-hidden="true"></i>'
+    }
+  
+})
+
+function formatTime(seconds){
+let mins=Math.floor(seconds/60);
+let secs=Math.floor(seconds%60);
+if(secs <  10)secs = '0' + secs
+return mins + ':' +secs
+}
+
+function updateSong(){
+    const recentSong=musicPlayer[currentSongIndex]
+    img.src=recentSong.images
+artistname.innerText=recentSong.artistName
+songtitle.innerText=recentSong.songTitle
+
+audio.src=recentSong.song
+
+audio.onloadedmetadata=()=>{
+progress.value=0
+duration.textContent=formatTime(audio.duration)
+progress.max=Math.floor(audio.duration)
+}
+}
+
+audio.addEventListener('timeupdate',()=>{
+currentTime.textContent=formatTime(audio.currentTime)
+progress.value=Math.floor(audio.currentTime)
+})
+
+progress.addEventListener('change',()=>{
+    audio.currentTime= progress.value
+    audio.play()
+     playBtn.innerHTML='<i class="fa fa-pause" aria-hidden="true"></i>'
+})
+
+function moveSlider(){
+    progress.value=audio.currentTime
+
+}
+setInterval(moveSlider,500)
+
+/*
+
+song.onloadedmetadata=function(){
+    songRange.max=song.duration;
+    songRange.value=song.currentTime;
+}
+
+
+play.addEventListener('click',()=>{
+   
+  song.play()
+pause.classList.remove('pause')
+play.classList.add('pause')
+
+    
+  
+})
+
+pause.addEventListener('click',()=>{
+   
+song.pause()
+pause.classList.add('pause')
+play.classList.remove('pause')
+ 
+   
+ 
+   
+
+})
+
+
+if(song.play()){
+    setInterval(()=>{
+ songRange.value=song.currentTime;
+    },500)
+}
+
+
+
+songRange.onchange=()=>{
+    song.play()
+    song.currentTime=songRange.value
+    pause.classList.remove('pause')
+play.classList.add('pause')
+}
+*/
